e9a47e6fddfe643cdaefec4595f9ca12ea197595
Sergey <101250956+herokiri@users.noreply.github.com>
2023-12-25 13:09:27 +0300

    Update urls.py

--- bookapp/books/urls.py
+++ bookapp/books/urls.py
@@ -1,26 +1,31 @@
-# ваш_проект/your_app/urls.py
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-
-from books import views
-from books.views import BookListView, BookDetailView, buy_book_view, register, user_login, book_detail, add_review, \
-    user_profile, create_genre, delete_book
+from books.views import (
+    CreateAuthorView,
+    CreatePublisherView,
+    CreateBookView,
+    AllBooksView,
+    BuyBookView,
+    CreateGenreView,
+    BookDetailView,
+    AddReviewView,
+    UserProfileView,
+    DeleteBookView,
+)
 
 urlpatterns = [
-    path('create-author/', views.create_author, name='create_author'),
-    path('create-publisher/', views.create_publisher, name='create_publisher'),
-    path('create-book/', views.create_book, name='create_book'),
-    path('', views.all_books, name='all_books'),
-    path('books/', BookListView.as_view(), name='book_list'),
-    path('books/<int:pk>/buy/', buy_book_view, name='buy_book'),
-    path('create-genre/', create_genre, name='create_genre'),
+    path('create-author/', CreateAuthorView.as_view(), name='create_author'),
+    path('create-publisher/', CreatePublisherView.as_view(), name='create_publisher'),
+    path('create-book/', CreateBookView.as_view(), name='create_book'),
+    path('', AllBooksView.as_view(), name='all_books'),
+    path('books/<int:pk>/buy/', BuyBookView.as_view(), name='buy_book'),
+    path('create-genre/', CreateGenreView.as_view(), name='create_genre'),
 
-    path('book/<int:pk>/', book_detail, name='book_detail'),
-    path('add_review/<int:pk>/', add_review, name='add_review'),
+    path('book/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('add_review/<int:pk>/', AddReviewView.as_view(), name='add_review'),
 
-    path('register/', register, name='register'),
-    path('login/', user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-    path('profile/', user_profile, name='user_profile'),
-    path('books/<int:book_pk>/delete/', delete_book, name='delete_book'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', UserProfileView.as_view(), name='user_profile'),
+    path('books/<int:book_pk>/delete/', DeleteBookView.as_view(), name='delete_book'),
 ]
